3bb7ee9d459699462775c17d6580b6aedab76ecc
samyoub-2004 <oubrahamsamy19@gmail.com>
2025-08-09 12:04:28 +0100

    making some changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // Configuration CORS mise à jour
 const allowedOrigins = [
-  'http://localhost:3000',
+  'http://localhost:3002',
   'https://gtickets-1.onrender.com'
 ];
 
